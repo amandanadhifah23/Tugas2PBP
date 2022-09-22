4e227945d65f754556d594eca3dfafd90839cf4c
Amanda Nadhifah <amandanadhifah8@gmail.com>
2022-09-22 10:51:09 +0700

    Add tugas 3

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve 
 
-# Create your tests here.
+class MyWatchlistApp(TestCase):
+    def test_url_html(self):
+        response = Client().get('/mywatchlist/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_xml(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_json(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from mywatchlist.models import MyWatchlistItem
 
-# Create your views here.
+from django.http import HttpResponse
+from django.core import serializers
+
 
 def show_mywatchlist(request):
-    data_watchlist = mywatchlist.objects.all()
+    data = MyWatchlistItem.objects.all()
     context = {
-    'list_movie': data_wathlist,
+    'list_movie': data,
     'nama': 'Amanda',
-    'Student ID': '2106637246',
+    'Student_ID': '2106637246',
 }
     return render(request, "mywatchlist.html", context)
+
+def show_mywatchlist_xml(request):
+    data = MyWatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_mywatchlist_json(request):
+    data = MyWatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
